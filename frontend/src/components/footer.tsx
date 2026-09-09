97b4bb26b7307c6f640ab8851acb95de9cbb254f
Minyemek Santana <minyemeksantanaarnold@gmail.com>
2026-09-09 10:58:45 +0200

    Premiere version du footer et du header

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,7 +1,100 @@
-function Footer() {
+import { Info, CalendarDays, Mail, Share2, House, FileQuestionMark, UserCog, ShoppingCart, Star, Headset, RotateCcw, Gavel, Shield, FileText } from "lucide-react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok } from "react-icons/fa";
+
+export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="bg-[#f8f9fa] text-[#212529] h-80">
+      <div className='mt-120 flex justify-between m-5  mb-14'>
+        <div className="mt-10">
+          <h1 className="text-2xl font-bold">A propos</h1>
+          <ul className="mt-3 space-y-2">
+            <li className="flex ">
+              <Info />
+              Qui sommes nous
+            </li>
+            <li className="flex ">
+              <CalendarDays />Tous les evenements</li>
+            <li className="flex">
+              <Mail />
+              Nous contacter
+            </li>
+          </ul>
+        </div>
+        <div className="mt-10">
+          <h1 className="text-2xl font-bold">Communaute</h1>
+          <ul className="mt-3 space-y-2">
+            <li className="flex">
+              <Share2 />
+              Nos reseaux sociaux
+            </li>
+            <li className="flex">
+              <House />
+              Point de vente physique
+            </li>
+            <li className="flex">
+              <FileQuestionMark />
+              FAQ
+            </li>
+          </ul>
+
+        </div>
+        <div className="mt-10">
+          <h1 className="text-2xl font-bold">Organisateurs</h1>
+          <ul className="mt-3 space-y-2">
+            <li className="flex">
+              <UserCog />
+              devenir Organisateurs
+            </li>
+            <li className="flex">
+              <ShoppingCart />
+              Places de marche
+
+            </li>
+            <li className="flex">
+              <Star />
+              Nos references
+            </li>
+          </ul>
+        </div>
+        <div className="mt-10">
+          <h1 className="text-2xl font-bold">Assistance</h1>
+          <ul className="mt-3 space-y-2">
+            <li className="flex">
+              <Headset />
+              Service client
+            </li>
+            <li className="flex">
+              <RotateCcw />
+              Politique de remboursement
+            </li>
+            <li className="flex">
+              <Gavel />
+              Mention legal
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <hr className="border my-3" />
+      <div className="flex justify-between text-[#212529] mt-4">
+
+        <ul className="flex ml-14 gap-5">
+          <li className="flex">
+            <Shield />
+            Politique de confidentialite</li>
+          <li className="flex">
+            <FileText />
+            Conditions general de vente
+          </li>
+          <li className="flex"> © 2026 TIKERAMA</li>
+        </ul>
+        <ul className='flex gap-3 mx-6 mt-3'>
+          <FaFacebook />
+          <FaInstagram />
+          <FaTiktok />
+          <FaLinkedin />
+        </ul>
+      </div>
+    </div>
   )
 }
-
-export default Footer
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,75 @@
-function Header() {
+import Image from 'next/image'
+import { CircleUserRound, UsersRound, Bell, Heart, Search, SlidersHorizontal, TextAlignJustify, MapPin, ChevronDown } from 'lucide-react'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok } from 'react-icons/fa'
+import Flag from 'country-flag-icons/react/3x2/AC'
+import { US } from 'country-flag-icons/react/1x1'
+
+
+
+
+export default function Header() {
   return (
-    <div>Header</div>
+    <>
+      <div className='flex justify-between  bg-[#f5f5f5] h-12'>
+        <div className='flex mx-6'>
+          <MapPin className='mt-3'/>
+          <select>
+            <option value=""> 
+              Cote d'ivoir
+            </option>
+            <option value="">
+              Guinee</option>
+            <option value="">Cameroun</option>
+            <option value="">Gabon</option>
+            <option value="">Senegal</option>
+            <option value="">Mali</option>
+            <option value="">Burkina Faso</option>
+            <option value="">Togo</option>
+            <option value="">Benin</option>
+            <option value="">Autre Pays</option>
+
+          </select>
+          {/* <ChevronDown /> */}
+        </div>
+        <div className='flex gap-3 mx-6 mt-3'>
+          <FaFacebook/>
+          <FaInstagram/>
+          <FaTiktok/>
+          <FaLinkedin/>
+        </div>
+
+      </div>
+      <div className='shadow-2xl h-17 border'>
+        <div className='flex justify-between bg-white'>
+          <div>
+            <Image src="/logo.png" alt='Logo' width={130} height={130} className='mt-4' />
+          </div>
+          <button className='absolute ml-70 mt-3'>
+            <SlidersHorizontal className='bg-[#f0151f] font-black text-white w-9 h-9 rounded-[12px] flex justify-center items-center' />
+          </button>
+
+          <input type="search" name="" id="" className='mt-2 relative  border border-black flex justify-center gap-1.25 items-center focus:outline-none  w-150 h-12 rounded-[12px] pl-12 ' />
+          <button className='absolute ml-208 mt-3'>
+            <Search className='bg-[#f0151f] text-white rounded-[12px]  w-9 h-9 shadow-2xs ' />
+          </button>
+          <button className='text-white rounded-4xl bg-[#f0151f] h-10 w-50 mt-3'>Publier un evenement</button>
+
+          <div className='flex space-x-4 '>
+            <button>
+
+              <UsersRound />
+            </button>
+            <button><Bell /></button>
+            <button><Heart /></button>
+            <button className='flex mt-4'>
+              <TextAlignJustify />
+              <CircleUserRound />
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </>
+
   )
 }
-
-export default Header
